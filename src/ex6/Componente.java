37436bc6134a11e3e59f566f164ef84355083caf
Diego de Melo Gonzaga <diego-gonzaga@live.com>
2017-11-28 07:49:44 -0200

    Adicionado classes básicas do negócio utilizando o padrão de projeto decorator.

---
+++ src/ex6/Componente.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex6;
+
+/**
+ *
+ * @author diego
+ */
+public abstract class  Componente {
+    private String Texto;
+
+    /**
+     * @return the Texto
+     */
+    public String getTexto() {
+        return Texto;
+    }
+
+    /**
+     * @param Texto the Texto to set
+     */
+    public void setTexto(String Texto) {
+        this.Texto = Texto;
+    }
+    
+}
