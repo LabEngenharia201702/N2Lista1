e2cfe516dad8e8f2d6690931be03519ba66b7229
Diego de Melo Gonzaga <diego-gonzaga@live.com>
2017-11-28 10:41:40 -0200

    Implementado classes Fabrica.

--- src/ex6/FabricaDecoracao.java
+++ src/ex6/FabricaDecoracao.java
@@ -9,6 +9,21 @@ package ex6;
  *
  * @author diego
  */
-public class FabricaDecoracao {
+public class FabricaDecoracao implements Fabrica{
+
+    @Override
+    public Componente criarComponente(String texto) {
+        Componente c=null;
+        if("CABECALHO".equals(texto.toUpperCase()))
+        {
+            c= new Cabecalho();
+        }   
+        else if("RODAPE".equals(texto.toUpperCase()))
+        {
+            c= new Rodape();
+        }
+            
+        return c;
+    }
     
 }
--- src/ex6/FabricaTicket.java
+++ src/ex6/FabricaTicket.java
@@ -13,7 +13,10 @@ public class FabricaTicket implements Fabrica{
 
     @Override
     public Componente criarComponente(String texto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Componente c=null;
+        if("TICKET".equals(texto.toUpperCase()))
+            c= new Ticket();
+        return c;
     }
     
 }
